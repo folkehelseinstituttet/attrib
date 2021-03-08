001709a1849752992de977e0fc7f550d6b0a15c5
Hofman <AuroraChristine.Hofman@fhi.no>
2021-03-08 16:07:18 +0100

    county fake death data

---
+++ R/gen_fake_death_county_data.R
@@ -0,0 +1,85 @@
+#' Fake data for mortalityregistration
+#'
+#' @format
+#' \describe{
+#' \item{doe}{Date og event}
+#' \item{dor}{Date of registration}
+#' \item{location_code}{Location code}
+#' }
+"data_fake_nowcasting_raw"
+
+
+# Generates fake data
+#
+# This function generates a fake dataset with parameters
+# @param n_locations Telling how many locations one wants in the output data, default = 11 the number of municipalities in Norway.
+
+
+gen_fake_death_data_county <- function() {
+  doe <- NULL
+  dor <- NULL
+  reg_lag <- NULL
+  . <- NULL
+
+  start_date <- as.Date("2018-01-01")
+  end_date <- as.Date("2020-01-01")
+
+  location_code <- c("county03", "county11", "county15", "county18", "county30", "county34", "county38", "county42", "county46", "county50", "county54")
+
+  skeleton <- expand.grid(
+    location_code = location_code,
+    date = seq.Date(
+      from = start_date,
+      to = end_date,
+      by = 7 # to get a weakly base.
+    ),
+    stringsAsFactors = FALSE
+  )
+  setDT(skeleton)
+
+  pop_data <- fhidata::norway_population_by_age_cats(cats = list(c(1:120)))[location_code %in% c("county03",
+                                                                                                 "county11",
+                                                                                                 "county15",
+                                                                                                 "county18",
+                                                                                                 "county30",
+                                                                                                 "county34",
+                                                                                                 "county38",
+                                                                                                 "county42",
+                                                                                                 "county46",
+                                                                                                 "county50",
+                                                                                                 "county54") & year == 2021]
+  pop_data[, pop_frac:= pop/5312753 ]
+
+  skeleton[, year := isoyear_n(date)]
+  skeleton[, week := isoweek(date)]
+  skeleton[, mu:=( 16.733681+ 0.046588*sin(2 * pi * (week) / 53) + 0.078491*cos(2 * pi * (week ) / 53) -0.012650* year) ]
+  skeleton[pop_data, on = c("location_code"), pop_frac:= pop_frac]
+  skeleton[pop_data, on = c("location_code"), pop:= pop]
+  skeleton[, deaths_exp:= round(exp(mu)*pop*1/7)]
+  skeleton[, deaths := rpois(.N, deaths_exp)]
+
+  temp_vec <- vector( "list", length = length(skeleton))
+
+  for (i in seq_along(skeleton)){
+
+    skeleton_temp <- expand.grid(
+      date = skeleton[i, date],
+      location_code = skeleton[i, location_code],
+      count = seq(1, skeleton[i, deaths], by = 1),
+      stringsAsFactors = FALSE
+    )
+    setDT(skeleton_temp)
+    temp_vec[[i]] <- as.data.table(skeleton_temp)
+  }
+
+  skeleton_death <- rbindlist(temp_vec)
+
+  skeleton_death[, doe := date]
+  skeleton_death[, reg_lag := stats::rpois(.N, 21)]
+  skeleton_death[, dor := doe + reg_lag]
+
+  # data_fake_nowcasting_county_raw <- skeleton_death[,.(doe, dor, location_code)]
+  # save(data_fake_nowcasting_county_raw, file = "data/data_fake_nowcasting_county_raw.rds", compress = "bzip2")
+
+  return(skeleton_death[,.(doe, dor, location_code)])
+}
